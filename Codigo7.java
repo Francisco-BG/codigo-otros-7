5b270cabd240fd3dd741070d13235bcedda9f66d
Francisco BG <frank.betgar@gmail.com>
2023-03-06 16:24:44 -0600

    ejercicio terminado

--- Codigo7.java
+++ Codigo7.java
@@ -1,39 +1,46 @@
-public class Código7 {
+//Es un programa que tiene guardadas las capitales de algunos países.
+//Le pide al usuario introducir el nombre e un país
+//Si tiene guardado el país, dice su capital, si no. Pide la capital para guardarla.
+//Este proceso se repite una y otra vez hasta que el usuario escriba "salir"
+import java.util.Scanner;//Se importó Scanner
+import java.util.HashMap;
+public class Codigo7{ //Se quitó el acento para evitar problemas
+  public static void main(String[] args){//Se agregó método Main
+    Scanner s = new Scanner(System.in);//Se agregó flujo de entrada
 
-    Scanner s = new Scanner();
+    HashMap<String, String> capitales = new HashMap<>();// Se cambió el tipo de clave y valor
 
-    HashMap<Byte, Double> ca = new HashMap<>();
-
-    capitales.put("Canadá", "Otawwa");
+    capitales.put("Canadá", "Ottawa");//correción ortográfica
     capitales.put("Estados Unidos", "Washington DC");
     capitales.put("México", "México DF");
     capitales.put("Belice", "Belmopán");
     capitales.put("Costa rica", "San José");
-    capitales.put("El Salvador");
+    capitales.put("El Salvador","San Salvador"); //se añadió capital
     capitales.put("Guatemala", "Ciudad de Guatemala");
     capitales.put("Honduras", "Tegucigalpa");
     capitales.put("Nicaragua", "Managua");
     capitales.put("Panamá", "Panamá");
-   
-    Sting c = "";
+    
+    String c = ""; //Le faltaba la erre a String 
 
     do {
-      System.in.print("Escribe el nombre de un país y te diré su capital: ");
-      c = s.nextDouble();
+      System.out.print("Escribe el nombre de un país y te diré su capital: ");// Cambio de in por out
+      c = s.nextLine();//Line es para recibir strings
       
-      if (!c.equal("salir")) {
-        if (ca.containsValue(c)) {
-          System.print("La capital de " + c);
-          System.out.println(" es " + ca.put(c));
-        } else {
+      if (!c.equals("salir")) { //se agregó la s a equal
+        if (capitales.containsKey(c)) { //cambio de nombre de variable, // cambio de Llave
+          System.out.print("La capital de " + c); // se agregó flujo de salida
+          System.out.println(" es " + capitales.get(c));//se cambió put por get, cambio de nombre de variable
+        }else{
           System.out.print("No conozco la respuesta ");
           System.out.print("¿cuál es la capital de " + c + "?: ");
-          String ca = s.nextLine();
-          ca.put(c, ca);
+          String newCa = s.nextLine();// cambio de nombre de variable para no tener conflictos con la preexistente
+          capitales.put(c, newCa); //cambio de ca por la nueva variable
           System.out.println("Gracias por enseñarme nuevas capitales");
         }
       }
-    } wile (!c.equals("salir");
+      
+    } while (!c.equals("salir"));//faltaba la hache de while
+    s.close();//se cierra scanner
   }
-
 }
